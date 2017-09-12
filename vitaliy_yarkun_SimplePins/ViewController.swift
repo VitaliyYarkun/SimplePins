48689b3c58404efe4f422b64729f255e95e2157c
Vitaliy Yarkun <vitaliy.yarkun@indeema.com>
2017-09-12 19:10:27 +0300

    Initial Commit

---
+++ vitaliy_yarkun_SimplePins/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  vitaliy_yarkun_SimplePins
+//
+//  Created by Vitaliy Yarkun on 9/12/17.
+//  Copyright © 2017 Vitaliy Yarkun. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
