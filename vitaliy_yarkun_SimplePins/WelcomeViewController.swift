196f21922d96fc8d5bb3ee86a460979e15cbb8b8
Vitaliy Yarkun <vitaliy.yarkun@indeema.com>
2017-09-15 00:17:22 +0300

    - Hide navigationbar on WelcomeVC;

--- vitaliy_yarkun_SimplePins/WelcomeViewController.swift
+++ vitaliy_yarkun_SimplePins/WelcomeViewController.swift
@@ -41,4 +41,15 @@ final class WelcomeViewController: UIViewController {
         }
     }
     
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        self.navigationController?.setNavigationBarHidden(true, animated: animated)
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        
+        self.navigationController?.setNavigationBarHidden(false, animated: animated)
+    }
 }
